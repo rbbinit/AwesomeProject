7b5fd3608fc9c6caf95adeff7a90dfe159bb9df4
hoit <krbbinit@gmail.com>
2020-07-15 21:46:51 +0900

    altitude first

--- App.js
+++ App.js
@@ -1,26 +1,44 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView} />
-      <View style={styles.blueView} />
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  yellowView: {
-    flex: 2,
-    backgroundColor: "yellow",
-  },
-  blueView: {
-    flex: 1,
-    backgroundColor: "blue",
-  },
-});
+//Object {
+//  "coords": Object {
+//    "accuracy": 13.859000205993652,
+//    "altitude": 56.79999923706055,
+//    "heading": 270.88116455078125,
+//    "latitude": 37.5435527,
+//    "longitude": 126.828619,
+//    "speed": 0.00909408275038004,
+//  },
+//  "mocked": false,
+//  "timestamp": 1594816955648,
+// }
---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>getting the fucking weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FFCE0B",
+  },
+  text: {
+    color: "#64748B",
+    fontSize: 23,
+  },
+});
